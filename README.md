0e2f2a0069ab758206748492f712d821313a4e30
RayLee18 <verylie02@gmail.com>
2023-02-23 09:17:15 +0700

    new

---
+++ README.md
@@ -0,0 +1,3 @@
+# tugas1_very_2020133006
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/tugas1_very_2020133006/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.tugas1_very_2020133006
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
